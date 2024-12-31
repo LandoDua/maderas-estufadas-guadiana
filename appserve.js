397ab28e6b263fe54d28d68dd3ed43ae73328e97
OrlandoDua <orlando.dua2@gmail.com>
2024-12-31 00:21:55 -0600

    CORS corregidos

--- appserve.js
+++ appserve.js
@@ -41,7 +41,7 @@ app.get('/', (req, res) => {
 app.use(morgan('dev')); // sirve para tener control de las peticiones
 app.use(express.json({ limit: '50mb'}))
 app.use(cookierParser())
-app.use(cors())
+app.use(cors({credentials: true}))
 
 app.use(express.urlencoded({
     extended: true,
